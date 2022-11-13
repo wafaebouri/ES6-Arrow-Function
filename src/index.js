b98932a940311bfa48e57be462e253d0c6956a01
wafaebouri <116091410+wafaebouri@users.noreply.github.com>
2022-11-12 20:43:30 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var numbers = [3, 56, 2, 48, 5];
+
+////Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map( x =>  x * x);
+// console.log(newNumbers);
+
+//////Filter - Create a new array by keeping the items that return true.
+const newNumbers = numbers.filter((num) => num < 10);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+var newNumber = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+
+////Find - find the first item that matches from an array.
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// })
+const newNumber2 = numbers.find((num) => num > 10);
+
+////FindIndex - find the index of the first item that matches.
+const newNumber3 = numbers.findIndex((num) => num > 10);
